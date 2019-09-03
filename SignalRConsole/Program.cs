c6e840c3d809a26b27008ad9b7eee476543b730f
13067827993 <767076381@qq.com>
2019-09-03 21:18:57 +0800

    添加项目文件。

---
+++ SignalRConsole/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SignalR.Client.Hubs;
+using SignalRConsole.SignalR.MessengerHub;
+using SignalR.Hosting.Self;
+using SignalR;
+using System.Threading.Tasks;
+
+namespace SignalRConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connection = new HubConnection("http://localhost:9616/");
+            for (int i = 0; i < 10; i++)
+            {
+                SignalRConsole.SignalR.MessengerHub.Message message = new SignalR.MessengerHub.Message();
+                message.Content = "Moo,你好吗";
+                message.Duration = 500;
+                message.Title = "Hello Title";
+                var myHub = connection.CreateProxy("messenger");
+                connection.Start().Wait();
+                Object[] myData = { (Object)message, "sourcing" };
+                myHub.Invoke("broadCastMessage", myData);
+            }
+            
+        }
+
+        
+    }
+}
